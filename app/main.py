40ecfd03201e18ae9eccdc6353f396f904bcf876
subhashhunter <subhash56190@gmail.com>
2025-09-29 00:37:14 +0530

    update main.py

--- app/main.py
+++ app/main.py
@@ -1,20 +1,17 @@
-
 import os
 from fastapi import FastAPI
 from app.routes import cases, metadata
 
 app = FastAPI(title="Lexi Backend Assignment")
 
-
 @app.get("/")
 async def root():
-    return {"message": "FastAPI server is running!"}
-
+return {"message": "FastAPI server is running!"}
 
 app.include_router(metadata.router)
 app.include_router(cases.router)
 
-if __name__ == "__main__":
-    import uvicorn
-    port = int(os.environ.get("PORT", 8000)) 
-    uvicorn.run("app.main:app", host="0.0.0.0", port=port, reload=True)
+if **name** == "**main**":
+import uvicorn
+port = int(os.environ.get("PORT", 8000))
+uvicorn.run("app.main:app", host="0.0.0.0", port=port, reload=True)
