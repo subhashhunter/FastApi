8345f0f5bd6d31b9482fc72b4b7af1cb09168587
subhashhunter <subhash56190@gmail.com>
2025-09-28 22:26:11 +0530

    change main.py

--- app/main.py
+++ app/main.py
@@ -13,3 +13,6 @@ async def root():
 app.include_router(metadata.router)
 app.include_router(cases.router)
 
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("app.main:app", host="0.0.0.0", port=8000, reload=True)
